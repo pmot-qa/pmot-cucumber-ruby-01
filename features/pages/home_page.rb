3402311bbc46e3385d420cb1f4d307ede2e9466b
Joseph Ghartey <joseph.ghartey79@gmail.com>
2016-11-21 13:51:57 +0000

    cleaning up files

--- features/pages/home_page.rb
+++ features/pages/home_page.rb
@@ -1,12 +1,11 @@
 class HomePage
- include PageObject
+  include PageObject
 
   page_url('http://www.ft.com')
-  link(:sign_inn, :css => '#o-header-nav-desktop [data-trackable = "Sign In"]')
-
+  link(:sign_in, :css => '#o-header-nav-desktop [data-trackable = "Sign In"]')
 
   def select_sign_in
-   sign_inn
+   sign_in
   end
 
 end
--- features/support/hook.rb
+++ features/support/hook.rb
@@ -1,11 +1,9 @@
 require 'watir-webdriver'
 
-
 Before do
   @browser = Watir::Browser.new :chrome
   @browser.driver.manage.window.maximize
 end
-
 
 After do
   @browser.close
